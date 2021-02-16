5ca4157863e5fd041d609fada3df08e5b30646e9
Lwazi Radebe <lwaziradebe100@gmail.com>
2021-02-16 19:50:59 +0200

    ReadMe and Ex01

---
+++ Module00/ex01/exec.py
@@ -0,0 +1,20 @@
+"""This program is supposed to reverse the order of a string, and the case of its
+words. If we have more than one argument we have to merge them into a single string and separate each arg by a ’ ’
+(space char)."""
+
+def exec(String):
+
+    Caps = " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+:><}{][/\\|?,\";.'"
+    Low = " abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+:><}{][/|\\?,"";.'"
+    NewString = ""
+
+    for i in String:
+        if i in Caps:
+            NewString += i.lower()
+        elif i in Low:
+            NewString += i.upper()
+
+    print(NewString[::-1])
+
+String = "Hello World!"
+exec(String)
