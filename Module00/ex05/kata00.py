5c4af10228c7beb400fb80afd716b047497feec1
Lwazi Radebe <lwaziradebe100@gmail.com>
2021-03-17 10:34:16 +0200

    First kata in exercise 5

---
+++ Module00/ex05/kata00.py
@@ -0,0 +1,10 @@
+def tuple_to_string(number1, number2, number3):
+    """This function takes numbers and inputs them into a tuple,
+    then prints the numbers back to a user"""
+
+#user defined numbers inserted in tuple
+    tuple = (number1, number2, number3)
+    print(f"The 3 numbers are: {tuple[0]} {tuple[1]} {tuple[2]}")
+
+
+tuple_to_string(19, 42, 21)
